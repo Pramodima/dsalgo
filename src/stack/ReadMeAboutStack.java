50efce4060b8949749ef21b975d2c533c61c2556
Pramodima <39489229+Pramodima@users.noreply.github.com>
2023-10-16 14:33:38 +0530

    ReadMeAboutStack.java

---
+++ src/stack/ReadMeAboutStack.java
@@ -0,0 +1,19 @@
+package stack;
+
+public class ReadMeAboutStack {
+    /*
+    Some real world application of stack
+
+
+    Text Editor: The undo feature in a any text editor is a classic example of stack. Whatever you do in a text editor is saved in a data structure and when you undo them, the same activities are undone based on Last In First Out Order (LIFO).
+    Web Browsers: Whatever you browse in a particular tab is saved in a stack, whenever you try to go back to the last visited page the tab stack outputs the last visited pages in last in first out order.
+    Balancing symbols while compilation of source code
+
+NearestLargerInLeft,NearestLargerInRight,NearestSmallerInLeft,NearestSmallerInRight
+
+These 4 problems are base for all other problems in stack.
+
+in case of dependent  for loops like j  depends on i, usually there is a chance that stack can be used. 
+
+    */
+}
